3baa429962934ddb6e87295250c022c548c679ba
Буланов Александр <alexbulanovgoodmail@gmail.com>
2026-02-13 15:01:01 +0300

    Добавлена анимация

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger, SplitText);
 
   const lenis = new Lenis();
 
@@ -10,4 +10,85 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 
   gsap.ticker.lagSmoothing(0);
+
+  const cardContainers = document.querySelectorAll(".card-container");
+
+  cardContainers.forEach((cardContainer) => {
+    const cardPaths = cardContainer.querySelectorAll(".svg-stroke path");
+    const cardTitle = cardContainer.querySelector(".card-title h3");
+
+    const split = SplitText.create(cardTitle, {
+      type: "words",
+      mask: "words",
+    });
+
+    gsap.set(split.words, { yPercent: 100 });
+
+    cardPaths.forEach((path) => {
+      const length = path.getTotalLength();
+      path.style.strokeDasharray = length;
+      path.style.strokeDashoffset = length;
+    });
+
+    let tl;
+
+    cardContainer.addEventListener("mouseenter", () => {
+      if (tl) tl.kill();
+      tl = gsap.timeline();
+
+      cardPaths.forEach((path) => {
+        tl.to(
+          path,
+          {
+            strokeDashoffset: 0,
+            attr: { "stroke-width": 250 },
+            duration: 2,
+            ease: "power2.out",
+          },
+          0,
+        );
+      });
+
+      tl.to(
+        split.words,
+        {
+          yPercent: 0,
+          duration: 0.75,
+          ease: "power3.out",
+          stagger: 0.075,
+        },
+        0.35,
+      );
+    });
+
+    cardContainer.addEventListener("mouseleave", () => {
+      if (tl) tl.kill();
+      tl = gsap.timeline();
+
+      cardPaths.forEach((path) => {
+        const length = path.getTotalLength();
+        tl.to(
+          path,
+          {
+            strokeDashoffset: length,
+            attr: { "stroke-width": 50 },
+            duration: 1,
+            ease: "power2.out",
+          },
+          0,
+        );
+      });
+
+      tl.to(
+        split.words,
+        {
+          yPercent: 100,
+          duration: 0.5,
+          ease: "power3.out",
+          stagger: { each: 0.05, from: "end" },
+        },
+        0,
+      );
+    });
+  });
 });
